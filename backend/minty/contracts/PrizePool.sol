68959035f0bae11b9dde876734277b1c36f8c220
Eddy G <eddy.spambari@gmail.com>
2023-06-29 01:06:21 -0700

    more withdraw functions, modifier adjustment, contract logic mostly done

--- backend/minty/contracts/PrizePool.sol
+++ backend/minty/contracts/PrizePool.sol
@@ -6,13 +6,14 @@ import "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
 contract PrizePool {
     address owner;
     address winner;
-    address[] allParticipants;
+    address[] public allParticipants;
 
     uint prizePool;
     uint endTime;
 
     bool public beginContest;
     bool public contestEnded;
+    bool possibleTie;
 
     //if there is a tie, then all funds are withdrawable to participant
     //if not, then all funds in this mapping are sent to prizePool
@@ -26,7 +27,7 @@ contract PrizePool {
         IERC721 nft;
         address owner;
         uint nftId;
-        uint numOfVotes;
+        uint numOfVotes; //can maybe utilize this
         bool submitted; //participants can only submit 1 NFT per contest
     }
     mapping(address => NFT) participants;
@@ -64,6 +65,7 @@ contract PrizePool {
     }
 
     modifier canVote() {
+        require(!contestEnded, "Contest has ended. No more votes are allowed");
         require(
             !voters[msg.sender],
             "You have already voted for a participant's NFT"
@@ -74,7 +76,7 @@ contract PrizePool {
         );
         require(
             !participants[msg.sender].submitted,
-            "You can't vote for your own NFT"
+            "Contest participants are not allowed to vote"
         );
         _;
     }
@@ -85,6 +87,15 @@ contract PrizePool {
             "Please wait until the voting period is over to withdraw your NFT"
         );
         require(participants[msg.sender].nftId != 0, "No NFT to withdraw");
+        _;
+    }
+
+    modifier canWithdrawLockedEther() {
+        require(
+            contestEnded,
+            "Please wait for the contest to end to withdraw your ether"
+        );
+        require(lockedEther[msg.sender] > 0, "No funds to withdraw");
         _;
     }
 
@@ -117,7 +128,6 @@ contract PrizePool {
         if (allParticipants.length == 5) {
             beginContest = true;
             endTime = block.timestamp + 1 days;
-            //createNFTContest();
         }
     }
 
@@ -126,9 +136,8 @@ contract PrizePool {
         lockedEther[msg.sender] += msg.value;
     }
 
-    function withdrawLockedEther() external {
-        require(lockedEther[msg.sender] > 0, "No funds to withdraw");
-
+    //only the voters have funds in these
+    function withdrawLockedEther() external canWithdrawLockedEther {
         uint balance = lockedEther[msg.sender];
         lockedEther[msg.sender] = 0;
         (bool sent, ) = payable(msg.sender).call{value: balance}("");
@@ -137,9 +146,27 @@ contract PrizePool {
 
     function withdrawPrizePool() external {
         require(msg.sender == winner, "You are not the winner");
+        require(!possibleTie, "This contest did not have a winner");
 
-        (bool sent, ) = payable(msg.sender).call{value: prizePool}("");
+        (bool sent, ) = payable(winner).call{value: prizePool}("");
         require(sent, "Withdraw failed, try again please");
+    }
+
+    //this is only callable if there was a tie. all submitters can withdraw their funds
+    function withdrawAllFunds() external {
+        require(
+            possibleTie,
+            "You cannot withdraw your funds because the contest had a winner"
+        );
+        require(
+            potentialWithdrawBalance[msg.sender] > 0,
+            "No funds to withdraw"
+        );
+
+        uint amount = potentialWithdrawBalance[msg.sender];
+        potentialWithdrawBalance[msg.sender] = 0;
+        (bool sent, ) = payable(msg.sender).call{value: amount}("");
+        require(sent, "Withdraw failed, try again");
     }
 
     function withdrawNFT() external canWithdrawNFT {
@@ -153,9 +180,28 @@ contract PrizePool {
     //this basically only exists for the purpose of testing so I don't have to
     //wait for the contest to end every time it starts lol
     function end() external onlyOwner {
-        //end contest and delete all participant and voter data. Full reset.
-    }
+        //lets check if there is a winner or of there is a tie!
+        uint highestVotes;
+        for (uint k; k < allParticipants.length; k++) {
+            if (participants[allParticipants[k]].numOfVotes > highestVotes) {
+                winner = allParticipants[k];
+                possibleTie = false;
+            } else if (
+                participants[allParticipants[k]].numOfVotes == highestVotes
+            ) {
+                possibleTie = true;
+            }
+        }
 
-    //??
-    function createNFTContest() internal {}
+        //if winner exists, send funds to prizePool
+        if (!possibleTie) {
+            uint amount;
+
+            for (uint k; k < allParticipants.length; k++) {
+                prizePool += potentialWithdrawBalance[allParticipants[k]];
+            }
+        }
+    }
 }
+
+//function createNFTContest() internal {}
